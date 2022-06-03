f0539e22385bac27b64019b364d9748cc3a7cc89
sophiagabriel <64995748+sophiagabriel@users.noreply.github.com>
2022-06-03 14:43:04 -0400

    Update app.py

--- app.py
+++ app.py
@@ -3,5 +3,5 @@ st.write("hello!")
 categories = ['a','b','c']
 st.multiselect("Pick an option", categories) 
 st.button("Click me!")
-if st.checkbox("Select me!")
+if st.checkbox("Select me!"):
   st.write("you selected the checkbox, congrats")
