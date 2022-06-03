fe16c8947735e0ed9adfe86506166dc545dadd52
sophiagabriel <64995748+sophiagabriel@users.noreply.github.com>
2022-06-03 14:44:13 -0400

    Update app.py

--- app.py
+++ app.py
@@ -2,6 +2,6 @@ import streamlit as st
 st.write("hello!")
 categories = ['a','b','c']
 st.multiselect("Pick an option", categories) 
-st.button("Click me!")
+st.sidebar.button("Click me!")
 if st.checkbox("Select me!"):
   st.write("you selected the checkbox, congrats")
